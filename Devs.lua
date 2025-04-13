490a6c982c60147790881d879d5da43b83d39a91
OnceTestAcc <110311720+OnceTestAcc@users.noreply.github.com>
2025-04-13 16:08:24 -0400

    Update Devs.lua

--- Devs.lua
+++ Devs.lua
@@ -3,7 +3,6 @@ return {
   "ySe^!n!y@qL!qGTFxkzHN6YJXm9QRn1JF",
   "y!qnLe^!@@S@qGTFxkzHN6YJXm9QRn1JF",
   "b%#K[Ynbun[uYRdB35t07lppvL3HnGBB9",
-  "b%buxsns%%uuYRdB35t07lppvL3HnGBB9",
   "yq[@S!q[L[^SqGTFxkzHN6YJXm9QRn1JF",
   "yqLy@^q^[@LL@GTFxkzHN6YJXm9QRn1JF"
 }
