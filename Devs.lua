5cedc3ff0ee320e2eabfdb7753c228ced4ba9ab3
Obisedor <obisedor1@outlook.com>
2025-06-15 19:04:07 +0200

    Update Devs.lua

--- Devs.lua
+++ Devs.lua
@@ -1,6 +1,6 @@
 return {
   "q[nLqe^n@n[eGTFxkzHN6YJXm9QRn1JF",
   "b%[#nbsxxsbnYRdB35t07lppvL3HnGBB9",
-  "y!qnLe^!@@S@qGTFxkzHN6YJXm9QRn1JF", -- silk
+  "y!qnLe^!@@S@qGTFxkzHN6YJXm9QRn1JF", -- silks main alt
   "b%[nxu%xbKbbxRdB35t07lppvL3HnGBB9" -- silk
 }
