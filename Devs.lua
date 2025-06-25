e3d9095880bf80b5efb862f067a507bc5ca3133f
Obisedor <obisedor1@outlook.com>
2025-06-25 22:44:21 +0200

    Update Devs.lua

--- Devs.lua
+++ Devs.lua
@@ -4,4 +4,5 @@ return {
   "y!qnLe^!@@S@qGTFxkzHN6YJXm9QRn1JF", -- silks main alt
   "b%[nxu%xbKbbxRdB35t07lppvL3HnGBB9", -- silks half throwaway
   "b%[nxxb#u%busRdB35t07lppvL3HnGBB9" -- silks current throwaway
+  "yq[eLLSy^L[SnGTFxkzHN6YJXm9QRn1JF" -- another silks half throwaway :3
 }
