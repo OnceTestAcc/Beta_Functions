704db67c5db472b959b70bf719d49e8240988685
OnceTestAcc <110311720+OnceTestAcc@users.noreply.github.com>
2025-06-30 17:23:52 -0400

    Update Devs.lua

--- Devs.lua
+++ Devs.lua
@@ -3,6 +3,6 @@ return {
   "yq[@S!@eSnyLnGTFxkzHN6YJXm9QRn1JF",
   "y!qnLe^!@@S@qGTFxkzHN6YJXm9QRn1JF", -- silks main alt
   "b%[nxu%xbKbbxRdB35t07lppvL3HnGBB9", -- silks half throwaway
-  "b%[nxxb#u%busRdB35t07lppvL3HnGBB9" -- silks current throwaway
+  "b%[nxxb#u%busRdB35t07lppvL3HnGBB9", -- silks current throwaway
   "yq[eLLSy^L[SnGTFxkzHN6YJXm9QRn1JF" -- another silks half throwaway :3
 }
