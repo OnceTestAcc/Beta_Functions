96c1229e83145ab977a86e1f1ff2a34517720ee6
OnceTestAcc <110311720+OnceTestAcc@users.noreply.github.com>
2025-11-15 16:08:09 -0500

    Update Devs.lua

--- Devs.lua
+++ Devs.lua
@@ -1,5 +1,5 @@
 return {
   "q[nLqe^n@n[eGTFxkzHN6YJXm9QRn1JF",
-  "yq@!^@^eLnSyyGTFxkzHN6YJXm9QRn1JF", -- rgie's alt
+  "y@q^qq@[LSLLnGTFxkzHN6YJXm9QRn1JF", -- rgie's alt
   "YY#ubbKn[n%YRdB35t07lppvL3HnGBB9" -- silk
 }
