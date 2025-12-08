7ac79f698fba05164a88f0590aaca5b868df77e5
OnceTestAcc <110311720+OnceTestAcc@users.noreply.github.com>
2025-12-08 16:31:01 -0500

    Update alternate account for rgie

--- Devs.lua
+++ Devs.lua
@@ -1,5 +1,5 @@
 return {
   "q[nLqe^n@n[eGTFxkzHN6YJXm9QRn1JF",
-  "y@n^ee!@ny[^qGTFxkzHN6YJXm9QRn1JF", -- rgie's alt
+  "y@n^eL^S[[LSyGTFxkzHN6YJXm9QRn1JF", -- rgie's alt
   "YY#ubbKn[n%YRdB35t07lppvL3HnGBB9" -- silk
 }
