27c35f884fa29dd1851f7fa8fccee717c9cedce0
OnceTestAcc <110311720+OnceTestAcc@users.noreply.github.com>
2026-07-20 03:34:16 -0400

    Update SupportedExecutors.lua

--- SupportedExecutors.lua
+++ SupportedExecutors.lua
@@ -3,44 +3,34 @@ return {
     "MacSploit",
     "Volt",
     "Potassium",
-    "Bunni",
     "Seliware",
-    "Volcano",
     "OpiumwareMac"
   },
   printer = {
      "MacSploit",
       "Volt",
       "Potassium",
-      "Bunni",
       "Seliware",
-      "Volcano",
       "OpiumwareMac"
   },
   truck = {
     "Volt",
     "Potassium",
-    "Bunni",
     "Seliware",
-    "Volcano",
     "OpiumwareMac"
   },
   revamp = {
     "MacSploit",
     "Volt",
     "Potassium",
-    "Bunni",
     "Seliware",
-    "Volcano",
     "OpiumwareMac"
   },
   wildwest = {
     "MacSploit",
     "Volt",
     "Potassium",
-    "Bunni",
     "Seliware",
-    "Volcano",
     "OpiumwareMac"
   }
 }
