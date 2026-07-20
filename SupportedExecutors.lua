61c0f77680488a68925e51179515248d41eaf411
OnceTestAcc <110311720+OnceTestAcc@users.noreply.github.com>
2026-07-19 21:22:01 -0400

    Create SupportedExecutors.lua

---
+++ SupportedExecutors.lua
@@ -0,0 +1,46 @@
+return {
+  anomicv1 = {
+    "MacSploit",
+    "Volt",
+    "Potassium",
+    "Bunni",
+    "Seliware",
+    "Volcano",
+    "OpiumwareMac"
+  },
+  printer = {
+     "MacSploit",
+      "Volt",
+      "Potassium",
+      "Bunni",
+      "Seliware",
+      "Volcano",
+      "OpiumwareMac"
+  },
+  truck = {
+    "Volt",
+    "Potassium",
+    "Bunni",
+    "Seliware",
+    "Volcano",
+    "OpiumwareMac"
+  },
+  revamp = {
+    "MacSploit",
+    "Volt",
+    "Potassium",
+    "Bunni",
+    "Seliware",
+    "Volcano",
+    "OpiumwareMac"
+  },
+  wildwest = {
+    "MacSploit",
+    "Volt",
+    "Potassium",
+    "Bunni",
+    "Seliware",
+    "Volcano",
+    "OpiumwareMac"
+  }
+}
