98616646f95a80123797ebb041817443ddf44869
OnceTestAcc <rgiecvtgn@outlook.com>
2026-08-02 16:52:32 -0400

    Initial commit

---
+++ Devs.lua
@@ -0,0 +1,5 @@
+return {
+  "q[nLqe^n@n[eGTFxkzHN6YJXm9QRn1JF",
+  "yLq!S@LSn!eLyGTFxkzHN6YJXm9QRn1JF", -- rgie's alt
+  "YY#ubbKn[n%YRdB35t07lppvL3HnGBB9" -- silk
+}
---
+++ SupportedExecutors.lua
@@ -0,0 +1,46 @@
+return {
+	anomicv1 = {
+		"MacSploit",
+		"Volt",
+		"Potassium",
+		"Seliware",
+		"OpiumwareMac",
+		"Real",
+		"Madium"
+	},
+	printer = {
+		"MacSploit",
+		"Volt",
+		"Potassium",
+		"Seliware",
+		"OpiumwareMac",
+		"Real",
+		"Madium"
+	},
+	truck = {
+		"MacSploit",
+		"Volt",
+		"Potassium",
+		"Seliware",
+		"OpiumwareMac",
+		"Real",
+		"Madium"
+	},
+	revamp = {
+		"MacSploit",
+		"Volt",
+		"Potassium",
+		"Seliware",
+		"OpiumwareMac",
+		"Real",
+		"Madium"
+	},
+	wildwest = {
+		"Volt",
+		"Potassium",
+		"Seliware",
+		"OpiumwareMac",
+		"Real",
+		"Madium"
+	}
+}
